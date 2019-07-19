82533939becd7699015f22888cd04cc19fc4a41d
thanhcong <tunghv00@gmail.com>
2019-07-19 20:40:58 +0700

    return detail error - input multi space - check second diff 0

--- week1-syntax.go
+++ week1-syntax.go
@@ -12,6 +12,7 @@ import (
 func eval(a float64, b float64, arithmetic string) (float64, error) {
 	var result float64 = 0
 	var err error = nil
+
 	switch arithmetic {
 	case "+":
 		result = a + b
@@ -20,6 +21,10 @@ func eval(a float64, b float64, arithmetic string) (float64, error) {
 	case "*":
 		result = a * b
 	case "/":
+		if 0 == int(b) {
+			err = errors.New("Second param must different 0")
+			break
+		}
 		result = a / b
 	default:
 		err = errors.New("Invalid arithmetic")
@@ -34,26 +39,26 @@ func main() {
 
 	scanner.Scan()
 	text := scanner.Text()
-	s := strings.Split(text, " ")
+	s := strings.Fields(text)
 	if len(s) != 3 {
-		fmt.Printf("ERROR\n")
+		fmt.Printf("ERROR: invalid input\n")
 		return
 	}
 
 	a, err1 := strconv.ParseFloat(s[0], 64)
 	if err1 != nil {
-		fmt.Printf("ERROR\n")
+		fmt.Printf("ERROR: first element must be float type\n")
 		return
 	}
 	b, err2 := strconv.ParseFloat(s[2], 64)
 	if err2 != nil {
-		fmt.Printf("ERROR\n")
+		fmt.Printf("ERROR: second element must be float type\n")
 		return
 	}
 
 	result, err3 := eval(a, b, s[1])
 	if err3 != nil {
-		fmt.Printf("ERROR\n")
+		fmt.Printf("ERROR: %s\n", err3)
 		return
 	}
 
