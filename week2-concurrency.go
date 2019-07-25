9b97bc391e51b5afed88504878e4629450588445
thanhcong <tunghv00@gmail.com>
2019-07-25 14:21:00 +0700

    read file

--- week2-concurrency.go
+++ week2-concurrency.go
@@ -3,15 +3,32 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
 )
 
-func countFrequencyAppears(filePath string) {
+func countFrequencyAppears(filePath string) (map[string]int, error) {
+	var result = make(map[string]int)
+
 	fileByte, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		
+		return result, err
 	}
+	
+	text := string(fileByte)
+	wordList := strings.Fields(text)
+	for _, w := range wordList {
+		if _, ok := result[w]; ok {
+			result[w]++
+		} else {
+			result[w] = 1
+		}
+	}
+
+	fmt.Println(result)
+
+	return result, nil
 }
 
 func main() {
-
+	countFrequencyAppears("a.txt")
 }
