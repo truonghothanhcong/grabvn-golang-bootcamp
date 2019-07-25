8a2dbf698dbed6c18f1ceeaf3c06983f453a8128
thanhcong <tunghv00@gmail.com>
2019-07-25 11:09:02 +0700

    add file

---
+++ week2-concurrency.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func countFrequencyAppears(filePath string) {
+	fileByte, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		
+	}
+}
+
+func main() {
+
+}
