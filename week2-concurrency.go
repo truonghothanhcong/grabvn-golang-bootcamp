e920a4315ec5fda818dac5a5d80470df0f7070b2
thanhcong <tunghv00@gmail.com>
2019-07-26 09:31:43 +0700

    improve by read file by block: still not done

--- week2-concurrency.go
+++ week2-concurrency.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"strings"
 	"sync"
@@ -11,6 +12,7 @@ import (
 
 var global_result = make(map[string]int)
 var numberOfJobs int = 3
+var numberOfBufferSizes int = 16
 
 func readAllFilePaths(pathFolder string) ([]string, error) {
 	var filePaths []string
@@ -71,6 +73,44 @@ func writer(writeResult <-chan map[string]int) {
 			}
 		}
 	}
+}
+
+// TODO: serve for large file by read file in block
+// not run now :)
+func countFrequencyAppears2(filePath string) (map[string]int, error) {
+	var result = make(map[string]int)
+	var buffer = make([]byte, numberOfBufferSizes)
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		return result, err
+	}
+	defer f.Close()
+	
+	for {
+		_, err := f.Read(buffer)
+		if err != nil {
+			fmt.Println(err)
+			if err == io.EOF {
+				break
+			}
+		} else {
+			text := string(buffer)
+			fmt.Println(text)
+		}
+	}
+	
+	// text := string(fileByte)
+	// wordList := strings.Fields(text)
+	// for _, w := range wordList {
+	// 	if _, ok := result[w]; ok {
+	// 		result[w]++
+	// 	} else {
+	// 		result[w] = 1
+	// 	}
+	// }
+
+	return result, nil
 }
 
 func main() {
